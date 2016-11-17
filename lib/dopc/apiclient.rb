a2da21f36901125360630724f5366d212ef1a1a6
Anselm Strauss <strauss@puzzle.ch>
2016-11-17 09:35:44 +0100

    added authentication, doc yet missing

--- lib/dopc/apiclient.rb
+++ lib/dopc/apiclient.rb
@@ -4,17 +4,20 @@ require 'rest-client'
 module Dopc
   class ApiClient
 
-    def initialize(base_url, api_version)
+    def initialize(base_url, api_version, auth_token = nil)
       @base_url = base_url
       @api_version = api_version
+      @auth_token = auth_token
     end
 
     def url(path)
       "#{@base_url}/api/v#{@api_version}#{'/' unless path.start_with?('/')}#{path}"
     end
 
-    def headers(headers = {})
-      {accept: :json, content_type: :json}.merge headers
+    def headers(opts = {})
+      headers = {accept: :json, content_type: :json}
+      headers.merge!({authorization: "Token token=\"#{@auth_token}\""}) if @auth_token
+      headers.merge(opts)
     end
 
     def request(method, path, payload = nil)
