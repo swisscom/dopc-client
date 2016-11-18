00dc26576aa596206613a03da2dbea23bd49bc9b
Anselm Strauss <strauss@puzzle.ch>
2016-11-18 15:42:45 +0100

    changed output format, showing started/duration times

--- lib/dopc/util.rb
+++ lib/dopc/util.rb
@@ -2,8 +2,8 @@ require 'time'
 
 module Dopc::Util
 
-  def self.ago(time)
-    ago = Time.now - Time.parse(time)
+  def self.ago(t1, t2 = Time.now)
+    ago = t2 - t1
     secs = ago.to_int
     mins = secs / 60
     hours = mins / 60
